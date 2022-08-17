a58feaf18689c18287b9689705a1d7b53a316dc2
Hicham1981 <hlamsatfigmail.com>
2022-08-17 14:52:51 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Morocco!'));
+app.get('/', (req, res) => res.send('Hello World from Morocco fi casa!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
